778e9d85907ed478671fce727d59fec70abc41f0
szczepulec <szczepan@letkiewicz.pl>
2026-01-04 11:55:35 +0100

    remove

--- Assignment 2/MatRANS/MatRANS/Examples/WaveBoundaryLayers/StreamingTurbulent/runAll.m
+++
@@ -1,7 +0,0 @@
-% Runs all the Holmedal & Myrhaug (2009) cases
-addpath ~/MatRANS/src/;
-cd Symmetric; unix('rm out_MatRANS.mat'); MatRANS; cd ..;
-cd Asymmetric; unix('rm out_MatRANS.mat'); MatRANS; cd ..;
-
-    
-    
--- Assignment 2/MatRANS/MatRANS/Examples/WavePlusCurrent/runAll.m
+++
@@ -1,5 +0,0 @@
-% Run all wave plus current simulations
-addpath ~/MatRANS/src;
-cd CurrentOnly; unix('rm out_MatRANS.mat'); MatRANS; cd ..;
-cd WaveOnly; unix('rm out_MatRANS.mat'); MatRANS; cd ..;
-cd Combined; unix('rm out_MatRANS.mat'); MatRANS; cd ..;
